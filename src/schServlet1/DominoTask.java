89022861d16c651865b706203e626e6fa8f8a59b
sjsalo <sjsalomonsen@yahoo.dk>
2019-04-03 19:10:55 +0200

    Version 1

---
+++ src/schServlet1/DominoTask.java
@@ -0,0 +1,47 @@
+package schServlet1;
+
+import java.util.TimerTask;
+import java.util.Date;
+import lotus.domino.*;
+
+public class DominoTask extends TimerTask {
+	Thread t = null;
+	Date now;
+	long howLong;
+	String dbName = "Test/Servlets.nsf";
+	String formName = "ServletCheck";
+	String viewName = "ServletSetup";
+	String servletName = "DominoTimer";
+
+	public void run() {
+		// As is the case with all classes that extend Thread or implement Runnable,
+		// they must override the public void run() method.
+		try {
+			now = new Date();
+			System.out.println(servletName + ": " + now.toString());
+			System.out.println("Threads #: " + t.activeCount());
+			NotesThread.sinitThread();
+			Session session = NotesFactory.createSession();
+			Database db = session.getDatabase(null, dbName);
+			Document doc = db.createDocument();
+			doc.appendItemValue("Form", formName);
+			doc.appendItemValue("ServletName", servletName);
+			doc.appendItemValue("StartTime", now.toString());
+			doc.save(true, false);
+			View view = db.getView(viewName);
+			Document doc1 = view.getDocumentByKey(servletName, true);
+			howLong = new Long(doc1.getItemValueInteger("SleepTime_d")).longValue();
+			if (howLong < 0) {
+				this.cancel();
+			}
+			/* Recycle objects */
+			doc.recycle();
+			doc1.recycle();
+			view.recycle();
+			NotesThread.stermThread();
+			System.runFinalization();
+		} catch (Exception e) {
+			System.out.println("Error: " + e.toString());
+		}
+	}
+}
---
+++ src/schServlet1/DominoTimer.java
@@ -0,0 +1,62 @@
+package schServlet1;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.util.Timer;
+import lotus.domino.*;
+
+public class DominoTimer extends HttpServlet {
+	String dbName = "Test/Servlets.nsf";
+	String formName = "ServletCheck";
+	String viewName = "ServletSetup";
+	String servletName = "ScheduledServlet";
+	long howLong;
+
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+	}
+
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, java.io.IOException {
+		response.setContentType("text/html");
+		java.io.PrintWriter out = response.getWriter();
+		out.println("Starting Domino Timer");
+		Timer timer = new Timer(false);
+		DominoTask dtask = new DominoTask();
+		timer.schedule(dtask, 0, processNotes());
+		out.close();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, java.io.IOException {
+		this.processRequest(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, java.io.IOException {
+		processRequest(request, response);
+	}
+
+	private long processNotes() {
+		try {
+			NotesThread.sinitThread();
+			Session session = NotesFactory.createSession();
+			Database db = session.getDatabase(null, dbName);
+			/* Get current wait time */
+			View view = db.getView(viewName);
+			Document doc1 = view.getDocumentByKey(servletName, true);
+			howLong = new Long(doc1.getItemValueInteger("SleepTime_d")).longValue();
+			/* Recycle objects */
+			doc1.recycle();
+			view.recycle();
+			NotesThread.stermThread();
+			System.runFinalization();
+			System.gc();
+		} catch (Exception notex) {
+			notex.printStackTrace();
+			System.err.println("Error: " + notex.toString());
+			System.err.println("Error: " + notex.getMessage());
+		}
+		return howLong;
+	}
+}
